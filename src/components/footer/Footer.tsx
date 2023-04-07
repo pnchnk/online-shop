e6e77c71372636d18747bfaf76879252b3081062
nikita_panchenko <pnchnk.nikita@gmail.com>
2023-04-07 14:05:37 +0300

    footer edited

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="py-5 bg-dark">
       <div className="container">
         <p className="m-0 text-center text-white">
-          Copyright &copy; Nikita's website 2022
+          Copyright &copy; Nikita's website 2023. Enjoy{':-)'}
         </p>
       </div>
     </footer>
